45694ccccec25b152a07be3adb3fd1dee36d2707
okmethod <okmethod2020@gmail.com>
2025-06-08 21:37:09 +0900

    :+1: adjust ChatEntriesVeiw

--- web-client/src/lib/components/ChatEntriesVeiw.svelte
+++ web-client/src/lib/components/ChatEntriesVeiw.svelte
@@ -13,7 +13,10 @@
     <div class:self-end={msg.role === "human"} class:self-start={msg.role === "ai"}>
       <span class="font-bold">{msg.role === "human" ? "You" : "Ai"}:</span>
       <div class="rounded bg-primary-900 p-2 min-w-lg max-w-[80%] border">
-        <span>{msg.content.text}</span>
+        <span class="text-primary-100">{msg.content.text}</span>
+        {#if isProcessing && msg.role === "ai" && index === chatEntries.length - 1 && !msg.content.text}
+          <span class="text-gray-400 ml-2">応答中...</span>
+        {/if}
         {#if msg.content.image}
           <div class="mt-1 flex justify-center">
             <img src={msg.content.image.url} alt="UploadedImage" class="w-40 rounded border" />
@@ -22,7 +25,4 @@
       </div>
     </div>
   {/each}
-  {#if isProcessing}
-    <div class="text-gray-400">応答中...</div>
-  {/if}
 </div>
